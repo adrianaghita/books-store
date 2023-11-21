735284915d049cc844d40ef420ef7ab9c39c970a
Adriana Ghita <a.adrianaghita@gmail.com>
2023-11-21 13:00:10 +0200

    Update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { BookProvider } from "./context/BooksContext";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import routes from "./routes/Routes";
+import { LoggedInProvider } from "./context/LoggedInContext";
+import { OrdersProvider } from "./context/OrdersContext";
+import { FavoritesBooksProvider } from "./context/FavoritesBooksContex";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <LoggedInProvider>
+        <OrdersProvider>
+          <BookProvider>
+            <FavoritesBooksProvider>
+              <ShoppingCartProvider>
+                <RouterProvider router={routes}></RouterProvider>
+              </ShoppingCartProvider>
+            </FavoritesBooksProvider>
+          </BookProvider>
+        </OrdersProvider>
+      </LoggedInProvider>
+    </>
+  );
 }
 
 export default App;
