009e9f63e503c76ce1f3dbb6e454c8b13c907906
Adriana Ghita <a.adrianaghita@gmail.com>
2024-02-05 16:24:52 +0200

    Implement Mobile Responsiveness for HomePage

---
+++ src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,7 @@
+import { slide as Menu } from "react-burger-menu";
+
+const BurgerMenu = () => {
+  return <Menu></Menu>;
+};
+
+export default BurgerMenu;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,11 +8,14 @@ import logo from "../../../src/assets/Logo/LogoITP.svg";
 import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 import { useLoggedInContext } from "../../context/LoggedInContext";
 import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const Header = () => {
   const { itemsCounter } = useShoppingCartContext();
   const { isLoggedIn, loggedEmail, logOut } = useLoggedInContext();
   const [loggedOutIsShown, setLogOutIsShown] = useState(false);
+  const [burgerMenuIsShown, setBurgerMenuIsShown] = useState(false);
 
   const toggleShowLogOutMenu = () => {
     setLogOutIsShown(!loggedOutIsShown);
@@ -109,7 +112,96 @@ const Header = () => {
             </NavLink>
           )}
         </li>
+
+        <button
+          className={styles[`burger-menu-icon`]}
+          onClick={() => setBurgerMenuIsShown(!burgerMenuIsShown)}
+        >
+          {burgerMenuIsShown ? (
+            <IoMdCloseCircle className={styles[`navBar-icon`]} />
+          ) : (
+            <GiHamburgerMenu className={styles[`navBar-icon`]} />
+          )}
+        </button>
       </ul>
+      {burgerMenuIsShown && (
+        <div className={styles[`menu-open`]}>
+          <ul>
+            <li>
+              <NavLink to={"/"}>
+                {" "}
+                <GoHome className={styles[`navBar-icon`]} />
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={"cart"}>
+                <CiShoppingCart className={styles[`navBar-icon`]} />
+                Shopping Cart
+                {itemsCounter > 0 && (
+                  <span className={styles[`items-counter`]}>
+                    {itemsCounter}
+                  </span>
+                )}
+              </NavLink>
+            </li>
+            <li>
+              {isLoggedIn ? (
+                <NavLink
+                  to={"orders"}
+                  className={({ isActive }) =>
+                    isActive ? styles[`active-link`] : styles[`inactive-link`]
+                  }
+                >
+                  <TbTruckDelivery className={styles[`navBar-icon`]} />
+                  Orders
+                </NavLink>
+              ) : (
+                <span className={styles[`link-disabled`]}>
+                  <TbTruckDelivery className={styles[`navBar-icon`]} />
+                  Orders
+                </span>
+              )}
+            </li>
+
+            <li>
+              {isLoggedIn ? (
+                <div className={styles.loggedin}>
+                  <button
+                    className={styles[`user-name`]}
+                    onClick={toggleShowLogOutMenu}
+                  >
+                    <MdOutlinePerson3 className={styles[`navBar-icon`]} />
+                    {loggedEmail}
+                  </button>
+                  <div
+                    className={
+                      loggedOutIsShown
+                        ? styles[`loggedin-menu`]
+                        : styles[`display-none`]
+                    }
+                  >
+                    <p className={styles[`favorite-list`]}>
+                      <Link to={"/favoriteList"}>Saved Items</Link>
+                    </p>
+                    <button onClick={logOut}>LOGOUT</button>
+                  </div>
+                </div>
+              ) : (
+                <NavLink
+                  to={"/logIn"}
+                  className={({ isActive }) =>
+                    isActive ? styles[`active-link`] : styles[`inactive-link`]
+                  }
+                >
+                  <MdOutlinePerson3 className={styles[`navBar-icon`]} />
+                  Login
+                </NavLink>
+              )}
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
